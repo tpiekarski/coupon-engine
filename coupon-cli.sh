5926583fcbbca26e229796cab291a9550b329c92
Thomas Piekarski <t.piekarski@deloquencia.de>
2017-11-26 10:13:36 +0100

    Fixing little issue in shell script launcher

--- coupon-cli.sh
+++ coupon-cli.sh
@@ -4,7 +4,7 @@ VERSION=0.0.2
 SHADED_JAR=coupon-prototype-${VERSION}-shaded.jar
 
 if [ -e target/${SHADED_JAR} ]; then
-    java -jar target/${SHADED_JAR} $*g
+    java -jar target/${SHADED_JAR} $*
 else
     echo -e "${SHADED_JAR} not found, aborting.\nRun maven to compile and package shaded jar."
 fi
