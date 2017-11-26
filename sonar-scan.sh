c9b82294f5dd255c9845b7f6e1de9525caa6a167
Thomas Piekarski <t.piekarski@deloquencia.de>
2017-11-26 08:46:50 +0100

    Replacing link with file for sonar-scan.sh shell script

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -1,1 +1,12 @@
-../scripts/sonar-scan-mvn.sh
+#!/bin/bash
+#
+# SonarQube Scanner with Code Coverage for Maven
+#
+
+echo -e "Running Maven...\n"
+mvn clean
+mvn validate
+mvn package
+
+echo -e "Running sonar-scanner...\n"
+sonar-scanner
